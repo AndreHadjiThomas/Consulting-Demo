2ced0783da6e5817bbd4d7c722efb91990c31c46
Andre Hadji-Thomas <andreht02@gmail.com>
2025-07-03 11:13:48 +0200

    Update Motor Oil Hellas_Renewable.py

--- pages/Motor Oil Hellas_Renewable.py
+++ pages/Motor Oil Hellas_Renewable.py
@@ -21,7 +21,7 @@ st.set_page_config(page_title="Wind-Farm Risk Viewer", layout="wide")
 # === USER CONFIG ===
 latitude = 37.75
 longitude = 22.41036
-radius_m = 7000
+radius_m = 5500
 positions = [
     "top_left", "top_center", "top_right",
     "left_center", "center", "right_center",
